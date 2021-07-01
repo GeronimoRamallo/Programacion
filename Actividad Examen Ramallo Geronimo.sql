84ec24d8bcf0e9f1169a304286fe24755ebebef4
GeronimoRamallo <81364667+GeronimoRamallo@users.noreply.github.com>
2021-07-01 09:59:36 -0300

    Delete Actividad Examen Ramallo Geronimo.sql

--- Actividad Examen Ramallo Geronimo.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE examen;
-
-CREATE TABLE estudiantes(
-	idest int not null primary key,
-	nombre varchar(12) not null,
-	apellido varchar(12) not null,
-	nota int not null,
-curso varchar(40) not null);
